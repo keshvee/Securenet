42de368eac2fe30f30a9faefc1a920f6e60b48ae
keshvee <keshveegungun@gmail.com>
2024-04-21 00:40:07 +0530

    init

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,9 @@ import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain='ak736.us.auth0.com'
-    clientId='PX8NOoQhXuaP230FDhCT4ZxL2ivKPcMg'
+<Auth0Provider
+    domain='keshvee-jain.us.auth0.com'
+    clientId='yfWaiTIESJwzhe9zbYX9NxLPR7gt9CM6'
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
